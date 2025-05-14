25504a8694573ecd48fbf85680e3953a4a023d0c
Aiden Huang <huang.a.aiden@gmail.com>
2025-05-13 22:12:03 -0700

    fix

--- components/GridBackground.tsx
+++ components/GridBackground.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useRef, useEffect, useMemo, useState, useCallback } from 'react';
 import { Canvas, useFrame, useThree } from '@react-three/fiber';
 import * as THREE from 'three';
